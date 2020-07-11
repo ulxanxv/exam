f3a5501f825a5522935671ad3c63caba60aea2f2
Ulxanxv <marzpg80@gmail.com>
2020-07-11 12:41:50 +0300

    ...

---
+++ src/with-webbrowser/with-webbrowser/Main.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace with_webbrowser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.infoPanelToolStrip = new System.Windows.Forms.ToolStrip();
+            this.fileMenuButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openFileButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutProgrammButton = new System.Windows.Forms.ToolStripButton();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.doItButton = new System.Windows.Forms.Button();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.xPointLabel = new System.Windows.Forms.Label();
+            this.yPointLabel = new System.Windows.Forms.Label();
+            this.closeDocButton = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.infoPanelToolStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 61);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(860, 388);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // infoPanelToolStrip
+            // 
+            this.infoPanelToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuButton,
+            this.toolStripSeparator1,
+            this.aboutProgrammButton});
+            this.infoPanelToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.infoPanelToolStrip.Name = "infoPanelToolStrip";
+            this.infoPanelToolStrip.Size = new System.Drawing.Size(884, 25);
+            this.infoPanelToolStrip.TabIndex = 1;
+            this.infoPanelToolStrip.Text = "toolStrip1";
+            // 
+            // fileMenuButton
+            // 
+            this.fileMenuButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileMenuButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileButton,
+            this.exitButton});
+            this.fileMenuButton.Image = ((System.Drawing.Image)(resources.GetObject("fileMenuButton.Image")));
+            this.fileMenuButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.fileMenuButton.Name = "fileMenuButton";
+            this.fileMenuButton.Size = new System.Drawing.Size(49, 22);
+            this.fileMenuButton.Text = "Файл";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(180, 22);
+            this.openFileButton.Text = "Открыть";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(180, 22);
+            this.exitButton.Text = "Выход";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // aboutProgrammButton
+            // 
+            this.aboutProgrammButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.aboutProgrammButton.Image = ((System.Drawing.Image)(resources.GetObject("aboutProgrammButton.Image")));
+            this.aboutProgrammButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.aboutProgrammButton.Name = "aboutProgrammButton";
+            this.aboutProgrammButton.Size = new System.Drawing.Size(86, 22);
+            this.aboutProgrammButton.Text = "О программе";
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "openFileDialog1";
+            // 
+            // doItButton
+            // 
+            this.doItButton.Location = new System.Drawing.Point(518, 484);
+            this.doItButton.Name = "doItButton";
+            this.doItButton.Size = new System.Drawing.Size(174, 37);
+            this.doItButton.TabIndex = 3;
+            this.doItButton.Text = "Рассчитать";
+            this.doItButton.UseVisualStyleBackColor = true;
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Location = new System.Drawing.Point(12, 467);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(219, 20);
+            this.xTextBox.TabIndex = 4;
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Location = new System.Drawing.Point(12, 493);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(219, 20);
+            this.yTextBox.TabIndex = 4;
+            // 
+            // xPointLabel
+            // 
+            this.xPointLabel.AutoSize = true;
+            this.xPointLabel.Location = new System.Drawing.Point(237, 470);
+            this.xPointLabel.Name = "xPointLabel";
+            this.xPointLabel.Size = new System.Drawing.Size(54, 13);
+            this.xPointLabel.TabIndex = 5;
+            this.xPointLabel.Text = "— точка X";
+            // 
+            // yPointLabel
+            // 
+            this.yPointLabel.AutoSize = true;
+            this.yPointLabel.Location = new System.Drawing.Point(237, 496);
+            this.yPointLabel.Name = "yPointLabel";
+            this.yPointLabel.Size = new System.Drawing.Size(54, 13);
+            this.yPointLabel.TabIndex = 5;
+            this.yPointLabel.Text = "— точка Y";
+            // 
+            // closeDocButton
+            // 
+            this.closeDocButton.Location = new System.Drawing.Point(698, 484);
+            this.closeDocButton.Name = "closeDocButton";
+            this.closeDocButton.Size = new System.Drawing.Size(174, 37);
+            this.closeDocButton.TabIndex = 6;
+            this.closeDocButton.Text = "Закрыть документ";
+            this.closeDocButton.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 459);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(884, 22);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(110, 17);
+            this.statusLabel.Text = "Статус расчета  — ";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 481);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.closeDocButton);
+            this.Controls.Add(this.yPointLabel);
+            this.Controls.Add(this.xPointLabel);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.doItButton);
+            this.Controls.Add(this.infoPanelToolStrip);
+            this.Controls.Add(this.webBrowser1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 520);
+            this.MinimumSize = new System.Drawing.Size(900, 520);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Экзамен";
+            this.infoPanelToolStrip.ResumeLayout(false);
+            this.infoPanelToolStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStrip infoPanelToolStrip;
+        private System.Windows.Forms.ToolStripDropDownButton fileMenuButton;
+        private System.Windows.Forms.ToolStripMenuItem openFileButton;
+        private System.Windows.Forms.ToolStripMenuItem exitButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton aboutProgrammButton;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.Button doItButton;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label xPointLabel;
+        private System.Windows.Forms.Label yPointLabel;
+        private System.Windows.Forms.Button closeDocButton;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
---
+++ src/with-webbrowser/with-webbrowser/Main.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace with_webbrowser
+{
+    public partial class Main : Form {
+
+        private String filePath;
+
+        public Main() {
+            InitializeComponent();
+
+            this.openFileButton.Click += OpenFileButton_Click;
+            this.closeDocButton.Click += CloseDocButton_Click;
+            this.doItButton.Click += DoItButton_Click;
+            this.aboutProgrammButton.Click += AboutProgrammButton_Click;
+            this.exitButton.Click += ExitButton_Click;
+
+            visibleComponents(false);
+        }
+
+        /// <summary>
+        /// Метод открытия файла
+        /// </summary>
+        private void openFile() {
+            if (fileDialog.ShowDialog() == DialogResult.OK) {
+                filePath = fileDialog.FileName;
+
+                webBrowser1.Navigate(new Uri(filePath));
+
+                changeSize(900, 600);
+                visibleComponents(true);
+            }
+        }
+
+        /// <summary>
+        /// Закрытие веб-сайта
+        /// </summary>
+        private void closeDoc() {
+            webBrowser1.Url = null;
+
+            changeSize(900, 520);
+            visibleComponents(false);
+        }
+
+        /// <summary>
+        /// Скрытие компонентов на форме
+        /// </summary>
+        /// <param name="what"></param>
+        private void visibleComponents(bool what) {
+            this.xTextBox.Visible = what;
+            this.yTextBox.Visible = what;
+
+            this.xPointLabel.Visible = what;
+            this.yPointLabel.Visible = what;
+
+            this.doItButton.Visible = what;
+            this.closeDocButton.Visible = what;
+        }
+
+        /// <summary>
+        /// Измнение размера окна
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        private void changeSize(int width, int height) {
+            this.MinimumSize = new Size(width, height);
+            this.MaximumSize = new Size(width, height);
+            this.Size = new Size(width, height);
+        }
+
+        /// <summary>
+        /// Метод расчета первого графика
+        /// </summary>
+        private void firstAlgorithm() {
+            int x, y;
+            // Координаты точки, вводимые пользователем
+            try {
+                x = Convert.ToInt32(xTextBox.Text);
+                y = Convert.ToInt32(yTextBox.Text);
+            } catch (FormatException) {
+                MessageBox.Show("Ошибка вводимых данных", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                xTextBox.Text = "";
+                yTextBox.Text = "";
+
+                statusLabel.Text = "Статус расчета — ошибка вводимых данных";
+                return;
+            }
+
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) == 1.0) ||
+                ((x == -1 || x == 0) && (y == 1 || y == -1))) {
+                MessageBox.Show("Точка лежит на границе!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else if ((Math.Pow(x, 2) + Math.Pow(y, 2) < 1) ||
+              ((x > -1 && x < 0) && (y < 1 && y > -1))) {
+                MessageBox.Show("Точка принадлежит заштрихованной области!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else {
+                MessageBox.Show("Точка не принадлежит заштрихованной области!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            statusLabel.Text = "Статус расчета — успешно";
+        }
+
+        /// <summary>
+        /// Метод расчета второго графика
+        /// </summary>
+        private void secondAlgorithm() {
+            /** АЛГОРИТМ РАСЧЕТА **/
+
+            int[] x = new int[4];
+            int[] y = new int[4];
+
+            // Координаты точки, вводимые пользователем
+            try {
+                x[0] = Convert.ToInt32(xTextBox.Text);
+                y[0] = Convert.ToInt32(yTextBox.Text);
+            } catch (FormatException) {
+                MessageBox.Show("Ошибка вводимых данных", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                xTextBox.Text = "";
+                yTextBox.Text = "";
+
+                statusLabel.Text = "Статус расчета — ошибка вводимых данных";
+                return;
+            }
+
+            // Координаты вершин треугольника (так как треугольник конкретный, то заполняются автоматически)
+            x[1] = -5;
+            y[1] = -3;
+            x[2] = 1;
+            y[2] = 4;
+            x[3] = 5;
+            y[3] = -4;
+
+            int a = (x[1] - x[0]) * (y[2] - y[1]) - (x[2] - x[1]) * (y[1] - y[0]);
+            int b = (x[2] - x[0]) * (y[3] - y[2]) - (x[3] - x[2]) * (y[2] - y[0]);
+            int c = (x[3] - x[0]) * (y[1] - y[3]) - (x[1] - x[3]) * (y[3] - y[0]);
+
+            if (a == 0 || b == 0 || c == 0) {
+                MessageBox.Show("Точка лежит на границе!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0)) {
+                MessageBox.Show("Точка принадлежит треугольнику!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else {
+                MessageBox.Show("Точка не принадлежит треугольнику!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            statusLabel.Text = "Статус расчета — успешно";
+        }
+
+        /// <summary>
+        /// Обработка кнопка "Рассчитать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DoItButton_Click(object sender, EventArgs e) {
+            if (filePath.Contains("fpage.html")) {
+                firstAlgorithm();
+            } else if (filePath.Contains("spage.html")) {
+                secondAlgorithm();
+            } else {
+                MessageBox.Show("Данная html страница незивества программе!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// Кнопка "Закрыть документ"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseDocButton_Click(object sender, EventArgs e) {
+            closeDoc();
+        }
+
+        /// <summary>
+        /// Кнопка "Открыть"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenFileButton_Click(object sender, EventArgs e) {
+            openFile();
+        }
+
+        /// <summary>
+        /// Кнопка "Выход"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Кнопка "О программе"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AboutProgrammButton_Click(object sender, EventArgs e) {
+            MessageBox.Show("\tВариант №11\r\n\tАвтор программы — Ульянов А.В. ПКсп-117", "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
